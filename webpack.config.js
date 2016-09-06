c023fd81e50e98ff9579115ccc4c893edba208ca
Chi To <williamto256@gmail.com>
2016-09-06 21:54:18 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeENV = process.env.NODE_ENV;
+
+module.exports = {
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		path: path.join(__dirname, 'app', 'build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{ test: /\.json$/, loader: 'json-loader' },
+			{ test:/\.css$/, loader: 'style-loader!css-loader' }
+		]
+	},
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	target: 'node',
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': { 'NODE_ENV': JSON.stringify(nodeENV) }
+		})
+	]
+}
