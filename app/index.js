b4e5dead3c6003e905af0ddce72a0abcdd125a67
Chi To <williamto256@gmail.com>
2016-09-11 19:56:52 +0700

    Changed renderer files to lib/

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './renderer/store';
-import Root from './renderer/Root';
+import store from '../lib/store';
+import Root from '../lib/Root';
 
 ReactDOM.render(
 	<Provider store={store}>
