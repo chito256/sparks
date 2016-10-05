dcfa27c4dfa4942545b95b9baba2a9ca631dbcc5
Chi To <williamto256@gmail.com>
2016-10-05 16:07:22 +0700

    Deprecated

--- app/menu/menus/help.js
+++ app/menu/menus/help.js
@@ -6,8 +6,6 @@ const help = {
     {
       label: 'About',
       click: (menuItem, browserWindow) => {
-        let aboutWindow = new BrowserWindow({width: 500, height: 280, resizable: false})
-        aboutWindow.loadURL(`file://${__dirname}/../../about.html`)
       }
     },
     {
@@ -16,7 +14,6 @@ const help = {
     {
       label: 'Report issues on GitHub',
       click: (menuItem, browserWindow) => {
-        shell.openExternal('https://github.com/sgleung/sparks/issues')
       }
     },
     {
@@ -25,13 +22,11 @@ const help = {
     {
       label: 'GitHub',
       click: (menuItem, browserWindow) => {
-        shell.openExternal('https://github.com/sgleung/sparks')
       }
     },
     {
       label: 'Twitter',
       click: (menuItem, browserWindow) => {
-        shell.openExternal('https://twitter.com/sgleung')
       }
     }
   ]
